feedb6f102c46eb88e45eed65e552e4bf5eab61c
JoViOli <jovioli.dev04@gmail.com>
2023-01-17 19:30:07 -0300

    Feat(Setup): initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HabitComponent } from './app/components/habits';
+import './shared/styles/global.css';
+
+function App() {
+
+  return (
+    <HabitComponent completed={2}/>
+  )
+}
+
+export default App
---
+++ src/app/components/habits/index.ts
@@ -0,0 +1,1 @@
+export { default as HabitComponent } from './Habits';
